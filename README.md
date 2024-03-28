8a42f86a95e8855de1956cff4e170293e54753dc
Fran Domovic <fran@calimero.network>
2024-03-28 21:08:40 +0100

    feat: e-voting relayer

---
+++ README.md
@@ -0,0 +1,65 @@
+### NEAR Protocol TypeScript Smart Contract
+
+This repository contains a TypeScript smart contract designed to run on the NEAR Protocol blockchain. The smart contract is implemented in TypeScript using the NEAR SDK.
+
+### Getting Started
+
+#### 1. Clone the Repository
+
+```bash
+$: git clone https://https://github.com/frdomovic/e-voting-relayer
+```
+
+#### 2. Install dependencies
+
+```bash
+$: npm install
+```
+
+#### 2. Add Environment variables to .env
+
+```bash
+CONTRACT_ACCOUNT_ID=""
+RELAYER_ACCOUNT_ID=""
+RELAYER_PRIVATE_KEY=""
+RELAYER_PUBLIC_KEY=""
+MONGO_DB_URI=""
+DATABASE_NAME=""
+COLLECTION_NAME=""
+ADMIN_API_KEY=""
+SERVER_PORT=3001
+```
+
+1. CONTRACT_ACCOUNT_ID - NEAR accountId where the voting smart contract is deployed (e.g. voting-contract.testnet)
+
+2. RELAYER_ACCOUNT_ID - NEAR accountId of relayer account
+
+   2.1. RELAYER_PRIVATE_KEY - NEAR privateKey of relayer account
+
+   2.2. RELAYER_PUBLIC_KEY - NEAR publicKey of relayer account
+
+   ```bash
+   $: near create-account relayer-account.testnet --useFaucet
+   > ~/.near-credentials/testnet/relayer-account.testnet.json
+   ```
+
+   Copy credentials from the .json
+   RELAYER_ACCOUNT_ID - e.g. relayer-account.testnet
+   RELAYER_PRIVATE_KEY - e.g. 2eQP...a8sw ()
+   RELAYER_PUBLIC_KEY - e.g. ed25519:62WU79rjHuyBT7dcE1iYBHEcamSkmURGoRbcNDYB65rV
+
+3. MONGO_DB_URI - Mongo database connection URI
+   [MongoDB](https://cloud.mongodb.com/) - go through process of setting up database.
+   Connect to cluster -> Connect your application (Drivers) -> Driver Node.js -> Version 5.5 or later -> copy connection url from step 3. on the page
+4. DATABASE_NAME - MongoDB database name
+5. COLLECTION_NAME - MongoDB collection name
+6. ADMIN_API_KEY - Secure API key (random 64 character password) e.g. 12nak2Wx12...
+   Prevents no web application API calls
+
+#### 4. Start relayer Server
+
+```bash
+$: npm install
+```
+
+#### 5. To run the whole project - Continue on readme.md for frontend
---
+++ relayer.js
@@ -0,0 +1,263 @@
+const http = require("http");
+const crypto = require("crypto");
+const big = require("bn.js");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const nearAPI = require("near-api-js");
+const { keyStores, KeyPair, connect, Contract } = nearAPI;
+
+require("dotenv").config();
+
+const CONTRACT_ACCOUNT_ID = process.env.CONTRACT_ACCOUNT_ID;
+const RELAYER_PRIVATE_KEY = process.env.RELAYER_PRIVATE_KEY;
+const RELAYER_ACCOUNT_ID = process.env.RELAYER_ACCOUNT_ID;
+const MONGO_DB_URI = process.env.MONGO_DB_URI;
+const DATABASE_NAME = process.env.DATABASE_NAME;
+const COLLECTION_NAME = process.env.COLLECTION_NAME;
+const SERVER_PORT = process.env.SERVER_PORT;
+
+const client = new MongoClient(MONGO_DB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const getNearObjects = async () => {
+  const myKeyStore = new keyStores.InMemoryKeyStore();
+  const keyPair = KeyPair.fromString(RELAYER_PRIVATE_KEY);
+  await myKeyStore.setKey("testnet", RELAYER_ACCOUNT_ID, keyPair);
+
+  const connectionConfig = {
+    networkId: "testnet",
+    keyStore: myKeyStore,
+    nodeUrl: "https://rpc.testnet.near.org",
+    walletUrl: "https://testnet.mynearwallet.com/",
+    helperUrl: "https://helper.testnet.near.org",
+    explorerUrl: "https://explorer.testnet.near.org",
+  };
+  const nearConnection = await connect(connectionConfig);
+
+  const relayerAccount = await nearConnection.account(RELAYER_ACCOUNT_ID);
+
+  const contract = new Contract(relayerAccount, CONTRACT_ACCOUNT_ID, {
+    viewMethods: [
+      "viewVotingKeys",
+      "viewVoters",
+      "viewVotingOptions",
+      "viewTimeLimits",
+    ],
+    changeMethods: ["addVotingKey", "castVote"],
+  });
+  return { relayerAccount, contract, myKeyStore };
+};
+
+const viewVotingOptions = async (contract) => {
+  let votingOptions = [];
+  try {
+    const inputArray = await contract.viewVotingOptions();
+    votingOptions = inputArray.map(([name, vote_count]) => ({
+      name,
+      vote_count,
+    }));
+    return JSON.stringify(votingOptions);
+  } catch (error) {
+    console.error(error);
+    return { error: "Could not view voting options" };
+  }
+};
+
+const viewTimeLimits = async (contract) => {
+  try {
+    const limits = JSON.parse(await contract.viewTimeLimits());
+
+    return JSON.stringify({
+      voteTime: limits.vote_time,
+      registerTime: limits.register_time,
+    });
+  } catch (error) {
+    console.error(error);
+    return { error: "Could not view voting options" };
+  }
+};
+
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+};
+
+const addKeysToContract = async (items, collection, relayerAccount) => {
+  try {
+    await collection.deleteMany({
+      _id: { $in: items.map((item) => item._id) },
+    });
+    shuffleArray(items);
+    items.forEach(async (item) => {
+      const voting_key = item.key;
+      if (voting_key) {
+        try {
+          await relayerAccount.functionCall({
+            contractId: CONTRACT_ACCOUNT_ID,
+            methodName: "addVotingKey",
+            args: {
+              _secret_key: voting_key,
+            },
+            gas: new big.BN("300000000000000"),
+          });
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const castVote = async (keyHash, votingOption, relayerAccount) => {
+  try {
+    const res = await relayerAccount.functionCall({
+      contractId: CONTRACT_ACCOUNT_ID,
+      methodName: "castVote",
+      args: {
+        _secret_key: keyHash,
+        _vote_option: votingOption,
+      },
+      gas: new big.BN("300000000000000"),
+    });
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+
+    const database = client.db(DATABASE_NAME);
+    const collection = database.collection(COLLECTION_NAME);
+
+    const server = http.createServer(async (req, res) => {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+
+      const { relayerAccount, contract } = await getNearObjects();
+
+      if (req.method === "GET" && req.url === "/health") {
+        res.end(JSON.stringify({ status: "running" }));
+      } else if (req.method === "GET" && req.url === "/viewVotingOptions") {
+        const response = await viewVotingOptions(contract);
+        res.end(JSON.stringify({ response: response }));
+      } else if (req.method === "GET" && req.url === "/viewTimeLimits") {
+        const response = await viewTimeLimits(contract);
+        res.end(JSON.stringify({ response: response }));
+      } else if (req.method === "POST" && req.url === "/addkey") {
+        let requestData = "";
+        req.setEncoding("utf8");
+        req.on("data", (chunk) => {
+          requestData += chunk;
+        });
+        req.on("end", async () => {
+          try {
+            const jsonData = JSON.parse(requestData);
+            if (jsonData.key) {
+              const existingItem = await collection.findOne({
+                key: jsonData.key,
+              });
+              if (!existingItem) {
+                await collection.insertOne({ key: jsonData.key });
+                console.log("Key added successfully");
+              } else {
+                console.log(`Item with key already exists.`);
+              }
+            }
+            const items = await collection.find().limit(10).toArray();
+            if (items.length >= 3) {
+              await addKeysToContract(items, collection, relayerAccount);
+            }
+            res.end(JSON.stringify({ response: true }));
+          } catch (error) {
+            console.error(error);
+            res.statusCode = 400;
+            res.end(
+              JSON.stringify({
+                response: false,
+                error: "Failed to process internal functions",
+              })
+            );
+          }
+        });
+      } else if (req.method === "POST" && req.url === "/adminAddKey") {
+        let requestData = "";
+        req.setEncoding("utf8");
+        req.on("data", (chunk) => {
+          requestData += chunk;
+        });
+        req.on("end", async () => {
+          try {
+            const jsonData = JSON.parse(requestData);
+            const adminAPIkey = jsonData.adminAPIKey;
+            if (adminAPIkey === process.env.ADMIN_API_KEY) {
+              const items = await collection.find().limit(10).toArray();
+              await addKeysToContract(items, collection, relayerAccount);
+              res.end(JSON.stringify({ response: true }));
+            } else {
+              res.statusCode = 401;
+              res.end(
+                JSON.stringify({ response: { error: "Invalid API key" } })
+              );
+            }
+          } catch (error) {
+            console.error(error);
+            res.statusCode = 400;
+            res.end(
+              JSON.stringify({ response: { error: "Invalid JSON data" } })
+            );
+          }
+        });
+      } else if (req.method === "POST" && req.url === "/castVote") {
+        let requestData = "";
+        req.setEncoding("utf8");
+        req.on("data", (chunk) => {
+          requestData += chunk;
+        });
+        req.on("end", async () => {
+          try {
+            const jsonData = JSON.parse(requestData);
+            const votingOption = jsonData.voting_option;
+
+            const response = await castVote(
+              jsonData.key,
+              votingOption,
+              relayerAccount
+            );
+            res.end(
+              JSON.stringify({
+                response: response.receipts_outcome[0].outcome.logs[0],
+              })
+            );
+          } catch (error) {
+            console.error(error);
+            res.statusCode = 400;
+            res.end(JSON.stringify({ response: { error: "Failed to vote" } }));
+          }
+        });
+      } else {
+        res.statusCode = 400;
+        res.end(JSON.stringify({ response: { error: "Endpoint not found" } }));
+      }
+    });
+
+    server.listen(SERVER_PORT, () => {
+      console.log(`Server is running on port ${SERVER_PORT}`);
+    });
+  } catch (err) {
+    console.error("Error:", err);
+  }
+}
+
+run().catch(console.dir);
