15d9038045e85e0cc712bbe125a04f86cd5e763f
Fran Domovic <fran@calimero.network>
2024-05-23 19:00:41 +0200

    fix: add cors headers

--- relayer.js
+++ relayer.js
@@ -144,6 +144,10 @@ async function run() {
     const server = http.createServer(async (req, res) => {
       res.statusCode = 200;
       res.setHeader("Content-Type", "application/json");
+      res.setHeader('Access-Control-Allow-Credentials', 'true');
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+      res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
 
       const { relayerAccount, contract } = await getNearObjects();
 
